8d5cbe73336942a0ee695c415faa62f8296d973b
alfiatul <alfiatulazizah25@gmail.com>
2016-02-02 09:23:07 +0700

    function getData pada table pelanggan

--- resources/views/partials/pelanggan/pelanggan.blade.php
+++ resources/views/partials/pelanggan/pelanggan.blade.php
@@ -1,11 +1,12 @@
 @extends('layouts.master')
 @section('title', 'Page Title')
 @section('content')
-
+<body onload="Index()">
     <div class="row">
         <div class="col-lg-12">
         </div>
     </div>
+    <div id="Index">
     <div class="row">
         <div class="col-lg-12">
             <div class="panel panel-default">
@@ -17,8 +18,7 @@
                 </button>
                 <div class="panel-body">
                     <div class="dataTable_wrapper">
-                        @if (count($pelanggan) > 0)
-                            <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                            <table class="table table-striped table-bordered table-hover" >
                                 <thead>
                                 <tr>
                                     <th>Nama</th>
@@ -29,32 +29,28 @@
                                     <th>Aksi</th>
                                 </tr>
                                 </thead>
-                                <tbody>
-                                @foreach ($pelanggan as $data)
-                                    <tr class="">
-                                        <td>{{ $data->name }}</td>
-                                        <td>{{ $data->alamat }}</td>
-                                        <td>{{ $data->noantrian }}</td>
-                                        <td>{{ $data->Jk }}</td>
-                                        <td>{{ $data->notel }}</td>
-                                        <td>
-                                            <button type="button" class="btn btn-outline btn-primary"
-                                                    onclick="location.href='/pelanggan/{{$data->id}}';">Detail
-                                            </button>
-                                            <button type="button" class="btn btn-outline btn-info"
-                                                    onclick="location.href='/edit-pelanggan/{{$data->id}}';">Edit
-                                            </button>
-                                            <button type="button" class="btn btn-outline btn-danger"
-                                                    onclick="location.href='/hapus-pelanggan/{{$data->id}}';">Delete
-                                            </button>
+                                <tbody id="tampildata">
+                                    {{--<tr class="">--}}
+                                        {{--<td>{{ $data->name }}</td>--}}
+                                        {{--<td>{{ $data->alamat }}</td>--}}
+                                        {{--<td>{{ $data->noantrian }}</td>--}}
+                                        {{--<td>{{ $data->Jk }}</td>--}}
+                                        {{--<td>{{ $data->notel }}</td>--}}
+                                        {{--<td>--}}
+                                            {{--<button type="button" class="btn btn-outline btn-primary"--}}
+                                                    {{--onclick="location.href='/pelanggan/{{$data->id}}';">Detail--}}
+                                            {{--</button>--}}
+                                            {{--<button type="button" class="btn btn-outline btn-info"--}}
+                                                    {{--onclick="location.href='/edit-pelanggan/{{$data->id}}';">Edit--}}
+                                            {{--</button>--}}
+                                            {{--<button type="button" class="btn btn-outline btn-danger"--}}
+                                                    {{--onclick="location.href='/hapus-pelanggan/{{$data->id}}';">Delete--}}
+                                            {{--</button>--}}
 
-                                        </td>
-                                    </tr>
-                                @endforeach
+                                        {{--</td>--}}
+                                    {{--</tr>--}}
                                 </tbody>
                             </table>
-
-                        @endif
                     </div>
                 </div>
                 <!-- /.panel-body -->
@@ -62,5 +58,244 @@
             <!-- /.panel -->
         </div>
     </div>
+    </div>
+    <div id="Create">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        Tambah Data Pelanggan
+                    </div>
+                    <div class="panel-body">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <form id="Form-Create">
+                                    <div class="form-group">
+                                        <label>Nama</label>
+                                        <label>:</label>
+                                        <input type="text" class="form-control" name="name">
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Alamat</label>
+                                        <label>:</label>
+                                        <input type="text" class="form-control" name="alamat">
+                                    </div>
+                                    <div class="form-group">
+                                        <label>No Antrian</label>
+                                        <label>:</label>
+                                        <input type="text" class="form-control" name="noantrian">
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Jenis Kelamin</label>
+                                        <label>:</label>
+                                        <input type="text" class="form-control" name="Jk">
+                                    </div>
+                                    <div class="form-group">
+                                        <label>No Telpon</label>
+                                        <label>:</label>
+                                        <input type="text" class="form-control" name="notel">
+                                    </div>
+                                    <div class="form-group">
+
+                                        <input class="btn btn-outline btn-info" type="submit" value="Simpan">
+                                        {{--onclick="location.href='/apoteker/{{ $data->id }}}';">Simpan--}}
+                                        <button type="button" class="btn btn-outline btn-primary"
+                                                onclick="Index()">Kembali
+                                        </button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+
+                    </div>
+                    <!-- /.panel-body -->
+                </div>
+                <!-- /.panel -->
+            </div>
+        </div>
+    </div>
+    <div id="Edit">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        Edit Data Pelanggan #
+                    </div>
+                    <div class="panel-body">
+                        {{--<form role="form">--}}
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <form id="Form-Edit">
+                                    <div class="form-group">
+                                        <label>Nama</label>
+                                        <label>:</label>
+                                        <input type="text" name="name" class="form-control">
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Alamat</label>
+                                        <label>:</label>
+                                        <input type="text" name="alamat" class="form-control">
+                                    </div>
+                                    <div class="form-group">
+                                        <label>No Antrian</label>
+                                        <label>:</label>
+                                        <input type="text" class="form-control" name="noantrian">
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Jenis Kelamin</label>
+                                        <label>:</label>
+                                        <input type="text" name="Jk" class="form-control">
+                                    </div>
+                                    <div class="form-group">
+                                        <label>No Telpon</label>
+                                        <label>:</label>
+                                        <input type="text" name="notel" class="form-control">
+                                    </div>
+                                    <div class="form-group">
+                                        <button type="submit" class="btn btn-outline btn-info">Simpan
+                                        </button>
+                                        <button type="button" class="btn btn-outline btn-primary"
+                                                onclick="Index()">Kembali
+                                        </button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                        {{--</form>--}}
+                    </div>
+                    <!-- /.panel-body -->
+                </div>
+                <!-- /.panel -->
+            </div>
+        </div>
+        <!-- /.row -->
+    </div>
+    <script src="{!! asset('bower_components/jquery/dist/jquery.min.js') !!}"></script>
+    <script>
+        $(document).ready(function () {
+            $('#Create').hide();
+            $('#Edit').hide();
+            getAjax();
+            $("#Form-Create").submit(function (event) {
+
+                event.preventDefault();
+                var $form = $(this),
+                        Nama = $form.find("input[name='name']").val(),
+                        Alamat = $form.find("input[name='alamat']").val(),
+                        No_Antrian = $form.find("input [name='noantrian']").val(),
+                        Jenis_Kelamin = $form.find("input[name='Jk']").val(),
+                        No_Telpon = $form.find("input[name='notel']").val();
+                //   $("From-Create").reset();
+
+                var posting = $.post('/apoteker', {
+                    name: Nama,
+                    alamat: Alamat,
+                    noantrian: No_Antrian,
+                    Jk: Jenis_Kelamin,
+                    notel: No_Telpon
+                });
+
+                //Put the results in a div
+                posting.done(function (data) {
+                    //console.log(data);
+                    window.alert(data.result.message);
+                    document.getElementById("From-Create").reset();
+                    location.reload();
+                    $('#Create').hide();
+                    $('#Edit').hide();
+                    $('#apoteker').show();
+                });
+            });
+        });
+
+
+        function Index() {
+            $('#Create').hide();
+            $('#Edit').hide();
+            $('#Index').show();
+        }
+        function Create() {
+            $('#Create').show();
+            $('#Edit').hide();
+            $('#Index').hide();
+        }
+
+        function getAjax() {
+            $("#tampildata").children().remove();
+            $.getJSON("/data-pelanggan", function (data) {
+                $.each(data.slice(0, 9), function (i, data) {
+                    $("#tampildata").append("<tr><td>" + data.name + "</td><td>" + data.alamat + "</td><td>" + data.noantrian + "</td><td>" + data.Jk + "</td><td>" + data.notel + "</td><td><button type='button' class='btn btn-outline btn-info' onclick='Edit(" + data.id + ")'>Edit</button><button type='button' class='btn btn-outline btn-danger' onclick='Hapus(" + data.id + ")'>Delete</button></td></tr>");
+                })
+            });
+        }
+        function Edit(id) {
+            $('#Create').hide();
+            $('#Edit').show();
+            $('#Index').hide();
+            $.ajax({
+                        method: "Get",
+                        url: '/pelanggan/' + id,
+                        data: {}
+                    })
+                    .done(function (data) {
+                        console.log(data.judul);
+                        //var $form = $(this),
+                        Nama = $("input[name='name']").val(data.name);
+                        Alamat = $("input[name='alamat']").val(data.alamat);
+                        No_Antrian = $("input[name='noantrian']").val(data.noantrian);
+                        Jenis_kelamin = $("input[name='Jk']").val(data.Jk);
+                        No_Telpon = $("input[name='notel']").val(data.notel);
+
+                        $('##Edit').show();
+                    });
+            $("Form-Edit").submit(function (event) {
+                event.preventDefault();
+                var $form = $(this),
+                        Nama = $form.find("input[name='name']").val(),
+                        Alamat = $form.find("input[name='alamat']").val(),
+                        No_Antrian = $form.find("input[name='noantrian']").val(),
+                        Jenis_Kelamin = $form.find("input[name='Jk']").val(),
+                        No_Telpon = $form.find("input[name='notel']").val();
+
+                $.ajax({
+                            method: "PUT",
+                            url: '/pelanggan/' + id,
+                            data: {
+                                Nama: name,
+                                Alamat: alamat,
+                                No_Antrian: noantrian,
+                                Jenis_Kelamin: Jk,
+                                NO_telpon: notel
+                            }
+                        })
+
+                        .done(function (data) {
+                            window.alert(data.result.message);
+                            getAjax();
+                            Index();
+                        });
+
+            });
+        }
+
+        function hapus(id) {
+            var result = confirm("Apakah Anda Yankin Ingin Menghapus ?");
+            if (result) {
+                $.ajax({
+                            method: "Delete",
+                            url: '/pelanggan/' + id,
+                            data: {}
+                        })
+
+                        .done(function (data) {
+                            window.alert(data.result.message);
+                            location.reload();
+                        });
+
+            }
+        }
+
+    </script>
+</body>
     <!-- /.row -->
 @endsection
